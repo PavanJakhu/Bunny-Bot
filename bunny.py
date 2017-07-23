b06e22acf1980a2c0c6a9e26a21487fbabc9972e
Pavan Jakhu <PavanJakhu@gmail.com>
2017-07-23 14:30:34 -0400

    Added 8ball Command and Clever Bot

--- bunny.py
+++ bunny.py
@@ -1,17 +1,23 @@
+import discord
+from discord.ext.commands import Bot
 import time
 import random
-import discord
 import json
-from discord.ext.commands import Bot
+import aiohttp
+import logging
 
 
 class Util:
     prevAccessPet = 0
     prevAccessCarrot = 0
+    prevAccess8Ball = 0
+    prevAccessCS = 0
+    prevCS = ""
     jsonData = None
     botCommandsChannel = None
 
 bunnyBot = Bot(command_prefix="bunbun.")
+client = aiohttp.ClientSession()
 util = Util()
 
 
@@ -27,7 +33,37 @@ async def on_ready():
 
 
 @bunnyBot.event
+async def on_command_error(error, ctx):
+    if ctx.message.content == "bunbun.8ball" and str(error) == "message is a required argument that is missing.":
+        await bunnyBot.send_message(ctx.message.channel, "You didn't ask a question!")
+    else:
+        raise error
+
+
+@bunnyBot.event
 async def on_message(msg):
+    #Clever Bot
+    for mention in msg.mentions:
+        if mention.name == bunnyBot.user.name:
+            if time.time() - util.prevAccessCS > 40:
+                util.prevCS = ""
+
+            params = {
+                "key": "CC3etFRu9AZtVw8ABtgEGiKvQ7w",
+                "input": msg.content,
+                "cs": util.prevCS
+            }
+
+            async with client.get("http://www.cleverbot.com/getreply", params=params) as r:
+                if r.status == 200:
+                    data = await r.json()
+
+                    util.prevCS = data["cs"]
+                    util.prevAccessCS = time.time()
+
+                    await bunnyBot.say(data["output"])
+
+    #Delete bad messages
     if msg.channel.name != "bot_commands":
         for x in Util.jsonData["bad words"]:
             if x in msg.content.casefold():
@@ -56,4 +92,19 @@ async def carrot():
         util.prevAccessCarrot = time.time()
 
 
+@bunnyBot.command(name="8ball", description="Ask Bunny a question and she'll answer using her magic ball!")
+async def _8ball(message : str):
+        if time.time() - util.prevAccess8Ball > 60:
+            if message.strip().endswith("?"):
+                await bunnyBot.say(random.choice(Util.jsonData["8ball sayings"]))
+                util.prevAccess8Ball = time.time()
+            else:
+                await bunnyBot.say("That's not a question! Try ending it with a \'?\'.")
+
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 bunnyBot.run("MjkzMTM0NTYyOTkxNTM4MTc2.C7zX_A.MTzQzRnqWm3Qaz7XRK9ZAV44qw4")
